28b1eaf2409a5197ad45cef8d38f8216d8591f8e
A7442 <parkjuna1121@naver.com>
2025-06-14 20:25:08 +0900

    feat : 스크립터블 오브젝트 수정 및 추가

--- Assets/02.Scripts/NPC/Data/NPCActionData.cs
+++ Assets/02.Scripts/NPC/Data/NPCActionData.cs
@@ -6,7 +6,7 @@ public enum ActionType {Move, Wait, Heal, Attack, Change}
 [CreateAssetMenu(menuName = "NPCAction/NPCActionData")]
 public class NPCActionData : ScriptableObject
 {
-    public List<NPCActionData> actions;
+    public List<NpcAction> actions;
 }
 
 [System.Serializable]
@@ -14,6 +14,6 @@ public class NpcAction
 {
     public ActionType type;
     public float duration;
-    public Vector3 targetPosition;
+    public float speed;
     public GameObject effectPrefab;
 }
