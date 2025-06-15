8d4595a5ccbf45c79984f49be3b83737c7bf2503
A7442 <parkjuna1121@naver.com>
2025-06-16 06:53:51 +0900

    feat : 프리팹 수정 및 기능 추가

--- Assets/02.Scripts/Map/Object/RuneStone.cs
+++ Assets/02.Scripts/Map/Object/RuneStone.cs
@@ -1,30 +1,43 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RuneStone : MonoBehaviour, IInteractable
 {
-    private bool canGoNextStage = true; // 다음 스테이지로 넘어갈 수 있는지 여부
-    private bool isPlayerInZone; // 플레이어가 영역에 있는지 여부
-    private Renderer renderer;
     public Material normalMaterial;
     public Material outLineMaterial;
     public GameObject interactGameObject; // 상호작용 오브젝트
 
+    [SerializeField] private DialogueData explainData;
+    
+    private bool canGoNextStage = true; // 다음 스테이지로 넘어갈 수 있는지 여부
+    private bool isPlayerInZone; // 플레이어가 영역에 있는지 여부
+    private Renderer renderer;
+    private UIManager uiManager;
+    private Queue<string> runeStoneQueue = new Queue<string>();
+    private PlayerInteract player;
+
     private void Start()
     {
+        player = FindObjectOfType<PlayerInteract>();
         renderer = GetComponent<Renderer>();
+        uiManager = UIManager.Instance;
+        foreach (string dialogue in explainData.dialogues)
+        {
+            runeStoneQueue.Enqueue(dialogue);
+        }
     }
 
     public void ShowInteractUI()
     {
-        
+        uiManager.interactableController.ShowInteractable(this.gameObject.layer);
     }
 
     public void InteractAction()
     {
         if (canGoNextStage && isPlayerInZone)
         {
+            uiManager.interactableController.HideInteractable();
+            ShowNextLine();
             OpenNextStage(); // 다음 스테이지로 넘어가는 메소드 호출
         }
     }
@@ -32,6 +45,29 @@ public class RuneStone : MonoBehaviour, IInteractable
     private void OpenNextStage()
     {
         interactGameObject.SetActive(false); // 다음 스테이지로 넘어갈 수 있도록 상호작용 오브젝트 비활성화
+    }
+    
+    private void ShowNextLine()
+    {
+        if (uiManager.dialogueController.IsTyping)
+        {
+            uiManager.dialogueController.CompleteCurrentLineInstantly();// 글자 다 안 나왔으면 바로 표시
+            return;
+        }
+
+        if (runeStoneQueue.Count == 0)
+        {
+            EndDialogue();
+        }
+        string line = runeStoneQueue.Dequeue();
+        uiManager.dialogueController.DisplayLine(line);//디알로그 출력
+    }
+
+    private void EndDialogue() //나중에 ESC키 같은 걸로 중간에 대사를 끊을 수 있을지도?
+    {
+        uiManager.dialogueController.HideDialoguePanel();
+        uiManager.interactableController.ShowInteractable(this.gameObject.layer);
+        player.OnEndInteraction();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
