8a2e72e81027899bb984b84cd7e42f233aa6404c
yoonjin1127 <129118497+yoonjin1127@users.noreply.github.com>
2025-06-17 07:37:18 +0900

    feat: GroundMonster 기초 스크립트 작성
    GroundMonster 기초 스크립트 작성

---
+++ Assets/02.Scripts/Enemy/StateMachine/GroundState/GroundIdleState.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundIdleState : IState
+{
+    private MonsterBase monster;
+
+    public GroundIdleState(MonsterBase monster)
+    {
+        this.monster = monster;
+    }
+
+    public void Enter()
+    {
+
+    }
+
+    public void Exit()
+    {
+
+    }
+
+    public void Update()
+    {
+
+    }
+}
