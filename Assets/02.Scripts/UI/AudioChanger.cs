28b17a15476ab78fe9c1dcfcb0aa33ff45346ac0
JangHoon Ban <janghoon333@naver.com>
2025-06-17 19:03:20 +0900

    feat : BGM 전용 콜라이더 추가

---
+++ Assets/02.Scripts/UI/AudioChanger.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioChanger : MonoBehaviour
+{
+    public AudioClip bossBGM;
+    private bool hasPlayed = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (hasPlayed) return;
+
+        if (collision.CompareTag("Player"))
+        {
+            AudioManager.Instance.PlayBGM()
+        }
+    }
+}
