d856b20c53b62c3ad1eb09455bc2d4b240c765b1
A7442 <parkjuna1121@naver.com>
2025-06-18 00:50:11 +0900

    feat : 인트로씬 구현

---
+++ Assets/02.Scripts/UI/IntroUI/IntroController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class IntroController : MonoBehaviour
+{
+    [SerializeField] private Image introImage;
+    [SerializeField] private TextMeshProUGUI introText;
+    [SerializeField] private List<IntroTextData> introTextData;
+
+    private int sceneIndex;
+    private float duration = 1.0f;
+    private Coroutine typingCoroutine;
+
+    private void Start()
+    {
+        introImage.color = new Color(1, 1, 1, 0);
+        sceneIndex = 0;
+        DisplayLine(introTextData[sceneIndex].sprite,introTextData[sceneIndex].introText);
+    }
+
+    public void DisplayLine(Sprite sprite, string line)
+    {
+        line = line.Replace("\\n", "\n");
+        introImage.sprite = sprite;
+        if (typingCoroutine != null)
+        {
+            StopCoroutine(typingCoroutine);
+        }
+
+        introImage.DOFade(1.0f, duration);
+        typingCoroutine = StartCoroutine(TypeLine(line));
+    }
+    
+    private IEnumerator TypeLine(string line)
+    {
+        StringBuilder sb = new StringBuilder();
+        introText.text = "";
+        introText.color = new Color(1, 1, 1, 1);
+        foreach (char c in line)
+        {
+            sb.Append(c);
+            introText.text = sb.ToString();
+            yield return new WaitForSeconds(0.05f);
+        }
+
+        yield return new WaitForSeconds(1.5f);
+        introImage.DOFade(0.0f, duration);
+        introText.DOFade(0.0f, duration);
+        sceneIndex++;
+        DisplayLine(introTextData[sceneIndex].sprite,introTextData[sceneIndex].introText);
+    }
+}
---
+++ Assets/02.Scripts/UI/IntroUI/IntroTextData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "IntroText/IntroTextData")]
+public class IntroTextData : ScriptableObject
+{
+    public Sprite sprite;
+    public string introText;
+}
